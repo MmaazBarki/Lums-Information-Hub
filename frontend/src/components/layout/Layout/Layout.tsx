4217e1034daed4b7b8804853c22d099b7ae0c8fb
Tammy <tammy@kali>
2025-04-08 17:19:33 +0500

    minor changes

--- frontend/src/components/layout/Layout/Layout.tsx
+++ frontend/src/components/layout/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Routes, Route, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import { 
   AppBar, 
