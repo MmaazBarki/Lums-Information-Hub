abee6d66c8669b08f8a21e4cddac657afdc1ab2b
farazahmad2004 <farazlhr09@gmail.com>
2025-04-08 17:48:54 +0500

    message implementation cloudinary

--- CHAT-APP/backend/src/lib/cloudinary.js
+++ CHAT-APP/backend/src/lib/cloudinary.js
@@ -1,28 +1,13 @@
-<<<<<<< HEAD
-import { v2 as cloudinary } from "cloudinary";
-
-import { config } from "dotenv";
-=======
 import {v2 as cloudinary} from "cloudinary";
 
 import {config} from "dotenv";
->>>>>>> 33df0c4f2a1f2b80e32fc691ad52c8c600d85721
 
 config();
 
 cloudinary.config({
-<<<<<<< HEAD
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-export default cloudinary;
-=======
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
->>>>>>> 33df0c4f2a1f2b80e32fc691ad52c8c600d85721
