7dfe80c7a0e112db0764d3421606e4544c4de8b6
farazahmad2004 <farazlhr09@gmail.com>
2025-04-08 20:47:22 +0500

    message implementation index.js

--- CHAT-APP/backend/src/index.js
+++ CHAT-APP/backend/src/index.js
@@ -6,11 +6,18 @@ import dotenv from "dotenv";
 import { connect } from "mongoose";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
 
 const PORT = process.env.PORT
+app.use(cors({
+    origin: ["http://localhost:5173", "http://localhost:3000"],  // Your frontend URLs
+    credentials: true,  // Allow cookies and credentials
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
 
 app.use(express.json());
 app.use(cookieParser());
