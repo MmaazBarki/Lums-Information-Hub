c12874c6ac2e9dda60ab9e4b7e9c359765e5f3f1
bilal-ateeq <26100254@lums.edu.pk>
2025-04-09 06:22:42 +0500

    course

---
+++ CHAT-APP/backend/src/models/course.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema({
+    course_code: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    course_name: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: false }); // No need for created_at/updated_at unless you want audit trail
+
+const Course = mongoose.model("Course", courseSchema);
+export default Course;
