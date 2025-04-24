82f3fb7ffc0b931257497bcbc95b0ba9b3767e65
bilal-ateeq <26100254@lums.edu.pk>
2025-04-24 18:06:56 +0500

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,15 +1,3 @@
-// import mongoose from "mongoose";
-
-// export const connectDB = async ()=>{
-//     try{
-//         const conn = await mongoose.connect(process.env.MONGOOSE_URI);
-//         console.log(`MongoDB connected: ${conn.connection.host}`)
-//     }catch(error){
-//         console.log("MongoDB connected error: ", error);
-
-//     }
-    
-// };
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
